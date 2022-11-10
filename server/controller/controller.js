873b724488433eb844ebbcdfac005c80b1319459
TJ <tj@mojaves-iMac.local>
2022-11-10 23:58:28 +0530

    create api is completed

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,4 +1,45 @@
-let Userdb=require('../model/model');
+let Userdb = require('../model/model');
 
 //create and save new user
-export
+exports.create = (req, res) => {
+    //validate request
+    if (!req.body) {
+        res.status(400).send({ message: "Content can not be empty" });
+        return;
+    }
+
+    //new user
+    const user = new Userdb({
+        name: req.body.name,
+        email: req.body.email,
+        gender: req.body.gender,
+        status: req.body.status,
+    })
+
+    //save user in the database
+    user.save(user)
+        .then(data => {
+            res.send(data)
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || "Some error occurred while creating a cretae operation"
+            });
+        })
+}
+
+// retrieve and return all users/retrive and return  a single user
+exports.find = (req, res) => {
+
+}
+
+//Update a new identified user by user id
+exports.update = (req, res) => {
+
+}
+
+// Delete a user with specified user id in th request
+exports.delete = (req, res) => {
+
+}
+
